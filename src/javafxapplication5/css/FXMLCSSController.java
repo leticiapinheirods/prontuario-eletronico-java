4ef8f9a4be124225f8ace68c756eb311afb43082
andressa <ribeiroandressa034@gmail.com>
2025-06-22 15:27:38 -0300

    projeto javafx

---
+++ src/javafxapplication5/css/FXMLCSSController.java
@@ -0,0 +1,17 @@
+
+package javafxapplication5.css;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class FXMLCSSController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
