9abc8ae33b36a3cae1383b6395c4239bcc735f88
Thais <thaisbitencourt6@gmail.com>
2025-06-29 14:37:30 -0300

    Update ServletInitializer.java
    comentário ServletInitializer.java

--- src/main/java/com/integracao/prontuarioeletronico/ServletInitializer.java
+++ src/main/java/com/integracao/prontuarioeletronico/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.integracao.prontuarioeletronico;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//Permitir que a aplicação Spring Boot seja inicializada dentro de um servidor de aplicação Java tradicional.
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
