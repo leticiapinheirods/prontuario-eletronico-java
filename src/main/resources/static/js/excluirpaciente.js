1c0e98d2a6c2269aaf27d6c90adc0b5d2ed3ec40
Thais <thaisbitencourt6@gmail.com>
2025-06-29 15:59:00 -0300

    Update excluirpaciente.js
    comentários excluirpaciente.js

--- src/main/resources/static/js/excluirpaciente.js
+++ src/main/resources/static/js/excluirpaciente.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const [btnSim, btnNao] = document.querySelectorAll("button");
+  const [btnSim, btnNao] = document.querySelectorAll("button"); //Seleciona os dois botões da página Sim e Não
 
-  btnSim.addEventListener("click", () => {
-    const paciente = JSON.parse(localStorage.getItem("pacienteSelecionado"));
+  btnSim.addEventListener("click", () => { // Se clicar no botão "Sim", confirmar exclusão
+    const paciente = JSON.parse(localStorage.getItem("pacienteSelecionado")); // Pega os dados do paciente armazenados no navegador
 
-    if (paciente && paciente.codigo) {
-      fetch(`http://localhost:8080/api/${paciente.codigo}`, {
+    if (paciente && paciente.codigo) { //Se o paciente existir e tiver código
+      fetch(`http://localhost:8080/api/${paciente.codigo}`, { //Envia pedido para o servidor excluir esse paciente
         method: "DELETE"
       })
-      .then(response => {
-        if (response.ok) {
+      .then(response => { 
+        if (response.ok) { // Se deu certo, remove o paciente do armazenamento local
           localStorage.removeItem("pacienteSelecionado");
-          window.location.href = "../html/excluirmensagem.html";
-        } else {
-          alert("Erro ao excluir paciente.");
+          window.location.href = "../html/excluirmensagem.html";  // Redireciona para a página que mostra mensagem de exclusão feita
+        } else { // Se houve erro ao excluir, exibe uma mensagem de alerta para o usuário
+          alert("Erro ao excluir paciente."); 
         }
       })
-      .catch(error => {
+      .catch(error => { //Se não conseguir se conectar com o servidor, mostra erro
         console.error("Erro:", error);
         alert("Erro na requisição." + error.message);
       });
     }
   });
-
+     //Se clicar no botão "Não" (cancelar exclusão) e volta para a página de informações do paciente
   btnNao.addEventListener("click", () => {
     window.location.href = "../html/informpaciente.html";
   });
