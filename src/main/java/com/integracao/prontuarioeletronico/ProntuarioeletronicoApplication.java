00ad8b2599a7018e200ea6c84b5acce22de5439c
Thais <thaisbitencourt6@gmail.com>
2025-06-29 14:33:15 -0300

    Update ProntuarioeletronicoApplication.java
    Comentário ProntuarioeletronicoApplication.java

--- src/main/java/com/integracao/prontuarioeletronico/ProntuarioeletronicoApplication.java
+++ src/main/java/com/integracao/prontuarioeletronico/ProntuarioeletronicoApplication.java
@@ -3,7 +3,7 @@ package com.integracao.prontuarioeletronico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication     //Classe que inicia toda a aplicação Spring Boot.
 public class ProntuarioeletronicoApplication {
 
 	public static void main(String[] args) {
